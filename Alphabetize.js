ec06b92fb1f0555a3bc2d3b7202a250684c8daf5
Daniil <danill.novikov@mail.ru>
2022-08-15 18:39:06 +0300

    add tasks

---
+++ Alphabetize.js
@@ -0,0 +1,10 @@
+// Напишите функцию alphabetize(str), которая возвращает строку, отсортировав её символы в алфавитном порядке.
+
+// function alphabetize(str) {
+//     return console.log(str.split('').sort().join(''));
+// }
+
+const alphabetize = (str) => console.log(str.split('').sort().join(''));
+
+alphabetize('gdpukiyit');
+alphabetize('acdb');
---
+++ Capital First Letter.js
@@ -0,0 +1,11 @@
+// Напишите функцию capitalizeFirstLetter(str), которая преобразовывает первый символ строки в верхний регистр.
+
+// function capitalizeFirstLetter(str) {
+//     return console.log(str[0].toUpperCase() + str.slice(1));
+// }
+
+const capitalizeFirstLetter = (str) => console.log(str[0].toUpperCase() + str.slice(1));
+
+capitalizeFirstLetter('hello')
+capitalizeFirstLetter('купить мыло')
+
