c170da84945ccbd2526a1795b7af140f7c1d6684
Daniil <danill.novikov@mail.ru>
2022-08-27 17:23:25 +0300

    add tasks

--- Filling an array.js
+++ Filling an array.js
@@ -1,17 +1,15 @@
 //Filling an array (part 1)
 // We want an array, but not just any old array, an array with contents!
-
 // Write a function that produces an array with the numbers 0 to N-1 in it.
-
 // For example, the following code will result in an array containing the numbers 0 to 4:
 
 const arr = (n) => {
-    let arr1 = [];
-    for (let i = 0; i <= n - 1; i++) {
-        arr1.push(i);
-    }
-    return arr1;
+  let arr1 = [];
+  for (let i = 0; i <= n - 1; i++) {
+    arr1.push(i);
+  }
+  return arr1;
 };
 
-console.log(arr(5));
-console.log(arr(4));
+console.log(arr(5)); // [ 0, 1, 2, 3, 4 ]
+console.log(arr(4)); //[ 0, 1, 2, 3 ]
