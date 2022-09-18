3f5beef2e4475c0295e2f627f65979c45583c2c3
Daniil <danill.novikov@mail.ru>
2022-09-19 00:16:49 +0300

    add task

--- Array Deep Count.js
+++ Array Deep Count.js
@@ -1,8 +1,10 @@
 function deepCount(arr) {
-
-    return arr.reduce((a, b) => a + (Array.isArray(b) ? deepCount(b) : 0), arr.length);
+  return arr.reduce(
+    (a, b) => a + (Array.isArray(b) ? deepCount(b) : 0),
+    arr.length
+  );
 }
 
 console.log(deepCount([1, 2, 3]));
-console.log(deepCount(["x", "y", ["z"]]));
+console.log(deepCount(['x', 'y', ['z']]));
 console.log(deepCount([1, 2, [3, 4, [5]]]));
